f94d79aa8547d91bc3c1a61d8f7f95422cef70f1
Viktor Fjällström <Viktor.DTK.Fjallstrom@lystkom.se>
2018-11-08 14:01:17 +0100

    js scope

--- projects/javascript-101/assets/js/4-function.js
+++ projects/javascript-101/assets/js/4-function.js
@@ -87,10 +87,10 @@ functions- return
 //Dessa argument används sedan inuti funktionen på något sätt -
 //Kanske matematiska beräkningar - kollar om värdet stämmer överens
 //Sedan returnerar funktionen ett värde, någon sorts output
-//
-//
-//
-//
+//Detta kallas för return a value - returnerar ett värde
+//Man kan alltså fånga upp värdet som funktionen ger
+//Och vidare i sin kod använda värdet av den funktionen för att använda 
+//På något annat sätt,
 
 function squared(x){
     return x * x;
@@ -98,8 +98,86 @@ function squared(x){
 console.log("4 squared is:" + squared(4));
 
 
-//Return  kan endast 
-//
-//
+//Return  kan endast returnera ett värde per funktion
+//Om man har två stycken rader med return, så kommer funktionen enbart att
+//returnera den första return som den stöter på
+
+
 var result = squared(10);
 console.log(result);
+
+/*
+***************************
+Funktions -muntligt test
+***************************
+*/
+
+// Test 1
+//Förklara vad som händer
+function test1(x, y){
+    return y - x;
+}
+
+console.log(test1(10, -40));
+
+/*
+******************************
+functions - skriftligt test
+******************************
+*/
+
+//Write a function isEven() which takes a single numeric argument and
+//returns true if the number is even, and false otherwise
+
+function isEven(x,){
+    if (x % 2 ==0 ){
+        return true;
+    }
+        else {
+            return false;
+        }
+}
+
+
+ 
+console.log(isEven(10))
+
+//Write a function kebabTosnake() which takes a single kebab-cased string
+//Argument and returns the snake_cased version
+//Basically replace "_" with "_"
+
+function kebabTosnake(str){
+    //replace all dashes with
+    var mystring = str.replace(/Wilford/g,"Master");
+    //return str
+    return mystring;
+    }
+
+kebabTosnake("Hej-jag-heter-Wilford");
+console.log(kebabTosnake("H-E-L-L"));
+
+/*
+**********************
+JS Scope
+**********************
+*/
+
+function localScope(){
+    var showScope = 31;
+    console.log(showScope);
+}
+localScope();
+console.log(showScope);
+
+var showScope = "I´m a Global hentai-master";//Global variable === accessible in the entire script
+console.log(showScope)
+
+var showScope = 99; // Global variabel
+function localScopeY(){
+    showScopeY = 100;
+    console.log(showScopeY);
+}
+console.log(showScopeY);//Global showscopeY
+localScopeY();
+console.log(showScopeY);
+
