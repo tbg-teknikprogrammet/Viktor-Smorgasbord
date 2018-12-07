b7bdafa19a5bd2c6aa491fedf4f6a17bcaff3225
Viktor Fjällström <Viktor.DTK.Fjallstrom@lystkom.se>
2018-12-07 11:10:52 +0100

    added comments and styled Todo more

--- projects/ToDo/assets/js/todo.js
+++ projects/ToDo/assets/js/todo.js
@@ -1,4 +1,4 @@
-
+// creates a button for every list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -9,7 +9,7 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-
+// Makes it possible for hiding the list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -19,7 +19,7 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-
+//Adds a checkmark to your list item on click
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -27,7 +27,7 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
-
+//Adds a new list item on click Add button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
@@ -45,7 +45,7 @@ function newElement() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
+//Removes a list item on the click of the "x"
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
