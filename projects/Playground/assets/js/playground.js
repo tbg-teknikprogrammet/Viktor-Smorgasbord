8c30f653f06cf2f45251e442b4f7cf3f521d6701
Viktor Fjällström <Viktor.DTK.Fjallstrom@lystkom.se>
2019-05-08 11:15:31 +0200

    fixade bilder

---
+++ projects/Playground/assets/js/playground.js
@@ -0,0 +1,8 @@
+var bg = document.getElementsByTagName("body")[0];
+var isBlue = false;
+
+//var toggle = getElementsByTagName("_")
+var Re2 = document.getElementById("Re2");
+Re2.addEventListener("click",function(){
+    bg.style.backgroundColor = "Grey";
+}); 
