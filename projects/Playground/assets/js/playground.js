9ec1d542c638b698bb3aa51d2b5ae19425e77685
Viktor Fjällström <Viktor.DTK.Fjallstrom@lystkom.se>
2019-05-27 11:36:43 +0200

    gjort mer på playground men för dum ned help!

--- projects/Playground/assets/js/playground.js
+++ projects/Playground/assets/js/playground.js
@@ -10,8 +10,32 @@ Re2.addEventListener("click",function(){
     //bg toggla en klass med bg färg
 });
 
+var p1Display = document.getElementById("P1Score")
+var p1Score = 0;
+var p2Display = document.getElementById("P2Score")
+var p2Score = 0;
+var playingTo = document.getElementById("playingTo")
+var inputNumber = document.getElementsByTagName("input")[0]
+var p1 = document.getElementById("p1")
+var p2 = document.getElementById("p2")
+var R = document.getElementById("R")
+var Win= document.getElementById("Winner")
+ 
+p1.addEventListener("click", function(){
+    p1Score++
+    p1Display.innerHTML = p1Score
+})
 
+p2.addEventListener("click", function(){
+    p2Score++
+    p2Display.innerHTML = p2Score
+})
+R.addEventListener("click", function(){
+    p1Score = 0
+    p1Display.innerHTML = p1Score
+})
+R.addEventListener("click",function(){
+    p2Score = 0
+    p2Display.innerHTML = p2Score
+})
 
-/*
-
-*/
